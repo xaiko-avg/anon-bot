d8bfb811efb6d4b6b20961649d0e04642fed607f
xaiko <kapalapimix101@gmail.com>
2024-07-09 00:24:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 TOKEN = os.environ.get("TOKEN",5916727319:AAGBj8x6AU50Y0cx0ThmOnDlm0XzTkgIkxQ "") # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER",6474577612 "kang_culiknew") # jangan dirubah agar tidak error
 GROUP = os.environ.get("GROUP",@Cari_pacar_jodoh_teman "") # 
-CHANNEL = os.environ.get("CHANNEL",@ofcbotxaiko "") # 
+CHANNEL = os.environ.get("CHANNEL",@ofcbotxaiko "vvslh_pro") # 
